65e1aa8de91bdf309d4d6d7bd6476fdc7cb61d51
banderhlog <grahpon@gmail.com>
2020-11-29 02:10:23 +0700

    Create settings.py
    Main settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+token = '1460310812:AAFd99DAf4ovprCrtPE0DDWyMpcipjtqeRs'
+# token = '864275050:AAE643eMQTo0gz1bkGUJyAWUEFDHZ8E9xMU'
+
+database_path = 'data_base.db'
+
+resumes_path_base = 'resumes/'
+
+tasks_path_base = 'test_tasks/'
+
+test_task_solution_path_base = 'test_task_solution/'
+
